0499b7217c43889a41fc98367ead0b8fb5aa5f75
Bimash Maharjan <87689921+bmasmhj@users.noreply.github.com>
2023-07-02 12:01:35 +0545

    userContext

---
+++ utils/UserContext.tsx
@@ -0,0 +1,22 @@
+import { type } from "os";
+import { createContext, useState, ReactNode } from "react";
+
+export type NavContextType = {
+    nav: boolean;
+    setNav: (nav: boolean) => void;
+};
+
+export const NavContext = createContext<NavContextType>({
+    nav: false,
+    setNav: () => { },
+});
+
+export const NavProvider = ({ children }: { children: ReactNode }) => {
+    const [nav, setNav] = useState(false);
+    return (
+        <NavContext.Provider value={{ nav, setNav }}>
+            {children}
+        </NavContext.Provider>
+    );
+}
+
