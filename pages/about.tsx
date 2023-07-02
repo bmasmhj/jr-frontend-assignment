d12a4c89b24595b9cd24ae66b33dbb5892d00da9
Bimash Maharjan <87689921+bmasmhj@users.noreply.github.com>
2023-07-02 12:01:12 +0545

    about

---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import MainLayout from "@/components/layout/MainLayout";
+export default function About({}) {
+  return (
+    <MainLayout title="About" description="Home page">
+      <h1>Music Sansar from home</h1>
+      <p>Sansar for music lovers</p>
+    </MainLayout>  
+  );
+}
