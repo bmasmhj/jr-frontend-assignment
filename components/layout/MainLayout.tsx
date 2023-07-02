240815dfc51f7605e5e5ae53fb0669d633325718
Bimash Maharjan <87689921+bmasmhj@users.noreply.github.com>
2023-07-02 12:00:41 +0545

    global layot

---
+++ components/layout/MainLayout.tsx
@@ -0,0 +1,30 @@
+// components/layout/MainLayout.tsx
+
+import Head from 'next/head';
+import NavBar from './NavBar';
+import Footer from './Footer';
+
+interface MainLayoutProps {
+  title: string;
+  description: string;
+  children: React.ReactNode;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = ({ title, description, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        {/* Add other head elements as needed */}
+      </Head>
+        <div className='bg-gray-100'>
+            <NavBar />
+            <main>{children}</main>
+        </div>
+    <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
