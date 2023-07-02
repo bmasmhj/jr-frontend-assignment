be2342f191290c50739f985c535369f8db7b01f1
Bimash Maharjan <87689921+bmasmhj@users.noreply.github.com>
2023-07-03 01:00:31 +0545

    space only

---
+++ components/common/SpaceOnly.tsx
@@ -0,0 +1,9 @@
+export default function SpaceOnly() {
+    return (
+        <div>
+          <p className="py-7 my-7 h-96"></p>  
+          <p className="py-7 my-7 h-96"></p>  
+          <p className="py-7 my-7 h-96"></p>  
+        </div>
+    )
+}
